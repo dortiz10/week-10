c04149e8e3fe1aaedc0c9cd3d77315e6bb67f0e1
David Ortiz <113551897+dortiz10@users.noreply.github.com>
2022-10-24 20:12:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from methods_help import method_help, greetings
+# from functions import function
+# from returnStuff import returnS
+
+method_help()
+greetings("Thomas")
+greetings("Same")
+greetings("Rebecca")
+# function()
+# returnS()
